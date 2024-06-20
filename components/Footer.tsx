106af2f88d8314b5b148ec3f288e956c20e16feb
Gufran Lazuardi <127754894+gufranlazuardi@users.noreply.github.com>
2024-06-20 03:25:00 +0000

    fix: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,17 @@
+import { Github, Linkedin, Mail } from "lucide-react";
 import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between">
-      <p className="text-sm font-light text-slate-400">
-        Copyright @2024 Gufran Lazuardi
+    <div className="flex justify-between items-center mt-[4rem]">
+      <p className="text-sm font-semibold text-slate-400">
+      © 2024 Gufran Lazuardi. All rights reserved
       </p>
-      <p className="text-sm font-light text-slate-400">
-        gufranlazuardi@gmail.com
-      </p>
+      <div className="flex gap-4">
+        <Github size={20}/>       
+        <Linkedin size={20} />
+        <Mail size={20} />
+      </div>
     </div>
   );
 };
