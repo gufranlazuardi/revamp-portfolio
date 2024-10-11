d797c88fb05d6b9f8eaabde681537525b6c0573d
gufranlazuardi <gufranlazuardi@gmail.com>
2024-10-12 01:04:39 +0700

    fix: remove unused variables

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
-import Link from "next/link";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import BlurEffects from "@/components/BlurEffects";
